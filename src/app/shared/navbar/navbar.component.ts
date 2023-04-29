da90a127fc7c10b278633c074a331eff365f63f2
Santiago Nivia <millernivia1301@gmail.com>
2023-04-28 23:45:40 -0500

    Animación bg scroll navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  @ViewChild('navbar') navbar!: ElementRef;
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any) {
+    const scrollPos = window.scrollY;
+    if (scrollPos > 100) {
+      console.log('pos');
+      this.navbar.nativeElement.classList.add('bg-dark');
+    } else {
+      console.log('pos2');
+      this.navbar.nativeElement.classList.remove('bg-dark');
+    }
+  }
+
+  constructor() {}
 
 }
